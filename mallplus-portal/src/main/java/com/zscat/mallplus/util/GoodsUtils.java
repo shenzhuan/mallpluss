9ae0c13b131af1f6d3bcafba50369798c06803e8
1439226817@qq.com <1439226817@qq.com>
2019-08-23 19:44:57 +0800

    商品评价

--- mallplus-portal/src/main/java/com/zscat/mallplus/util/GoodsUtils.java
+++ mallplus-portal/src/main/java/com/zscat/mallplus/util/GoodsUtils.java
@@ -4,6 +4,7 @@ import com.zscat.mallplus.pms.entity.PmsProduct;
 import com.zscat.mallplus.pms.vo.SamplePmsProduct;
 import org.springframework.beans.BeanUtils;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,5 +26,28 @@ public class GoodsUtils {
         SamplePmsProduct en = new SamplePmsProduct();
         BeanUtils.copyProperties(list, en);
         return en;
+    }
+
+    public static void main(String[] args) {
+        // create 2 BigDecimal Objects
+        BigDecimal bg1, bg2;
+
+        bg1 = new BigDecimal(123.044);
+        bg2 = new BigDecimal("-1.123");
+
+        // create two int objects
+        int i1,i2;
+
+        // assign the result of scale on bg1, bg2 to i1,i2
+        i1 = bg1.scale();
+        i2 = bg2.scale();
+
+        String str1 = "The scale of " + bg1 + " is " + i1;
+        String str2 = "The scale of " + bg2 + " is " + i2;
+
+        // print the values of i1,i2;
+        System.out.println( str1 );
+        System.out.println( str2 );
+
     }
 }
