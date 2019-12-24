a1530b8bba89f717690f3e0457dfd441697a1be7
1439226817@qq.com <1439226817@qq.com>
2019-12-24 10:01:38 +0800

    拼团

--- mallplus-admin/src/main/java/com/zscat/mallplus/config/MybatisPlusConfig.java
+++ mallplus-admin/src/main/java/com/zscat/mallplus/config/MybatisPlusConfig.java
@@ -1,26 +1,16 @@
 package com.zscat.mallplus.config;
 
 
-import com.baomidou.mybatisplus.core.parser.ISqlParser;
-import com.baomidou.mybatisplus.core.parser.ISqlParserFilter;
-import com.baomidou.mybatisplus.core.parser.SqlParserHelper;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.tenant.TenantHandler;
-import com.baomidou.mybatisplus.extension.plugins.tenant.TenantSqlParser;
 import com.zscat.mallplus.ApiContext;
 import com.zscat.mallplus.enums.ConstansValue;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.LongValue;
-import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.reflection.MetaObject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.ArrayList;
 import java.util.List;
 
 //Spring boot方式
@@ -41,54 +31,7 @@ public class MybatisPlusConfig {
         PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
 
         paginationInterceptor.setDialectType("mysql");
-        /*
-         * 【测试多租户】 SQL 解析处理拦截器<br>
-         * 这里固定写成住户 1 实际情况你可以从cookie读取，因此数据看不到 【 麻花藤 】 这条记录（ 注意观察 SQL ）<br>
-         */
-        List<ISqlParser> sqlParserList = new ArrayList<>();
-        TenantSqlParser tenantSqlParser = new TenantSqlParser();
-        tenantSqlParser.setTenantHandler(new TenantHandler() {
 
-            @Override
-            public Expression getTenantId() {
-                // 从当前系统上下文中取出当前请求的服务商ID，通过解析器注入到SQL中。
-                Long currentProviderId = apiContext.getCurrentProviderId();
-                if (null == currentProviderId) {
-                    currentProviderId = 1l;
-                    System.out.println("#1129 getCurrentProviderId error.");
-                    //  throw new RuntimeException("#1129 getCurrentProviderId error.");
-                }
-                return new LongValue(currentProviderId);
-            }
-
-            @Override
-            public String getTenantIdColumn() {
-                return "store_id";
-            }
-
-            @Override
-            public boolean doTableFilter(String tableName) {
-                if (tableName.startsWith("admin_") || tableName.startsWith("QRTZ_")) {
-                    return true;
-                }
-                return IGNORE_TENANT_TABLES.stream().anyMatch((e) -> e.equalsIgnoreCase(tableName));
-
-            }
-        });
-
-        sqlParserList.add(tenantSqlParser);
-        paginationInterceptor.setSqlParserList(sqlParserList);
-        paginationInterceptor.setSqlParserFilter(new ISqlParserFilter() {
-            @Override
-            public boolean doFilter(MetaObject metaObject) {
-                MappedStatement ms = SqlParserHelper.getMappedStatement(metaObject);
-                // 过滤自定义查询此时无租户信息约束【 麻花藤 】出现
-                if ("com.zscat.mallplus.sys.mapper.SysUserMapper.selectByUserName".equals(ms.getId())) {
-                    return true;
-                }
-                return false;
-            }
-        });
         return paginationInterceptor;
     }
 
