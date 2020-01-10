1bc05701492dc82c8f0db4a5a68c0d50305aaf74
1439226817@qq.com <1439226817@qq.com>
2020-01-10 18:50:07 +0800

    商品查询

--- mallplus-mbg/src/main/java/com/zscat/mallplus/enums/ConstansValue.java
+++ mallplus-mbg/src/main/java/com/zscat/mallplus/enums/ConstansValue.java
@@ -16,7 +16,7 @@ public class ConstansValue {
             "        delete_status, publish_status, new_status, recommand_status, verify_status, sort, sale, price, promotion_price,\n" +
             "        original_price, stock, low_stock, store_name,store_id,unit,\n" +
             "        weight, preview_status, service_ids,  brand_name,\n" +
-            "        product_category_name, supply_id, create_time, school_id";
+            "        product_category_name, supply_id, create_time, school_id,area_id";
 
     public static final String sampleOrderList = "id,member_id, order_sn, create_time, member_username, total_amount, pay_amount,status, order_type,pay_type,source_type,goods_id,goods_name";
 
